3e41666f13b4a7f46fff62f2c6067ad28f68a0b6
constantinandrei <andrei.todirascu@gmail.com>
2024-01-05 07:52:20 +0100

    interactions for basic nft

--- src/BasicNft.sol
+++ src/BasicNft.sol
@@ -5,8 +5,9 @@ import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract BasicNft is ERC721 {
     uint256 private s_tokenCounter;
-    mapping (uint256 => string) private s_tokenIdToUri;
-    constructor () ERC721("Dogie", "DOG") {
+    mapping(uint256 => string) private s_tokenIdToUri;
+
+    constructor() ERC721("Dogie", "DOG") {
         s_tokenCounter = 0;
     }
 
@@ -16,7 +17,9 @@ contract BasicNft is ERC721 {
         s_tokenCounter++;
     }
 
-    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+    function tokenURI(
+        uint256 tokenId
+    ) public view override returns (string memory) {
         return s_tokenIdToUri[tokenId];
     }
 }
--- test/BasicNftTest.t.sol
+++ test/BasicNftTest.t.sol
@@ -8,8 +8,30 @@ import {DeployBasicNft} from "../script/DeployBasicNft.s.sol";
 contract BasicNftTest is Test {
     DeployBasicNft public s_deployBasicNft;
     BasicNft public s_basicNft;
+    address public USER = makeAddr("user");
+    string public constant PUG_URI =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+
     function setUp() public {
         s_deployBasicNft = new DeployBasicNft();
         s_basicNft = s_deployBasicNft.run();
+    }
+
+    function testNameIsCorrect() public view {
+        string memory expectedName = "Dogie";
+        string memory actualName = s_basicNft.name();
+        assert(
+            keccak256(abi.encodePacked(actualName)) ==
+                keccak256(abi.encodePacked(expectedName))
+        );
+    }
+
+    function testCanMintAndHaveABalance() public {
+        vm.prank(USER);
+        s_basicNft.mintNft(PUG_URI);
+        uint256 expectedBalance = 1;
+        uint256 actualBalance = s_basicNft.balanceOf(USER);
+        assert(actualBalance == expectedBalance);
+        assert(keccak256(abi.encodePacked(s_basicNft.tokenURI(0))) == keccak256(abi.encodePacked(PUG_URI)));
     }
 }
